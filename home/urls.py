860af8d736623c68851f971beb172f9e1c3349b7
anuran <anuranroy02@gmail.com>
2021-09-06 17:20:26 +0530

    First prototype

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', views.index, name="Home"),
+    path('about/', views.about, name="About"),
+    path('team/', views.team, name="Team"),
+]
